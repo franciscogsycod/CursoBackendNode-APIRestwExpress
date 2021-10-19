c9945912a3c86296d577eea51d1fe451bef0c147
franciscogsycod <francisco.gomez@sycod.com>
2021-10-19 12:18:03 -0500

    Routing de /products (JSON)

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (request, response) => {
   response.send("Hola este es mi server en Express");
 });
 
+app.get('/products', (request, response) => {
+  response.send({
+    id: 1,
+    name: 'Celular',
+    price: 14000
+  });
+});
+
 app.listen(port, () => {
   console.log(`Mi puerto esta en ${port}`);
 });
