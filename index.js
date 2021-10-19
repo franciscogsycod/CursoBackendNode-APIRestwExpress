2ea7600baeec2f79757461847c70b065e73fe3a8
franciscogsycod <francisco.gomez@sycod.com>
2021-10-19 12:13:00 -0500

    Echando a andar el servidor de Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("My App");
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (request, response) => {
+  response.send("Hola este es mi server en Express");
+});
+
+app.listen(port, () => {
+  console.log(`Mi puerto esta en ${port}`);
+});
+
